c774a4f81d711d20b893ac27061f384de18b3b2a
junhee <112875165+MINLUV@users.noreply.github.com>
2023-05-12 03:38:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.59 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,8 +2,7 @@ def solution(participant, completion):
     answer = ''
     participant.sort()
     completion.sort()
-    for listadd in range(len(participant)-len(completion)):
-        completion.append("0")
+    completion.append("0")
     for i in range(len(participant)):
         if completion[i] != participant[i]:
             answer+= participant.pop(i)
