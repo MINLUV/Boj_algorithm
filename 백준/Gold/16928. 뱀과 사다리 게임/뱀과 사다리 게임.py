14d68b26321379da5da12616d47e433c317e9c56
junhee <112875165+MINLUV@users.noreply.github.com>
2024-09-10 17:45:14 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 60 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,30 +1,29 @@
 import collections
-
-x, y = map(int, input().split())
+x , y = map(int,input().split())
 
 laddersnake = dict()
-for i in range(x + y):
-    a, b = map(int, input().split())
+
+queue = collections.deque()
+for i in range(x+y):
+    a,b = map(int,input().split())
     laddersnake[a] = b
 
 mincount = float('inf')
-queue = collections.deque()
-visited = set()  # 방문한 위치를 기록할 set
-queue.append((1, 0))
-visited.add(1)  # 시작 위치를 방문한 것으로 표시
-
+queue.append((1,0))
+visited = set()
+visited.add(1)
 while queue:
-    now, count = queue.popleft()
+    now , count = queue.popleft()
     if now >= 100:
         mincount = count
         break
-    else:
-        for i in range(1, 7):
-            next_pos = now + i
-            if next_pos in laddersnake:
-                next_pos = laddersnake[next_pos]
-            if next_pos not in visited:  # 방문하지 않은 위치만 추가
-                visited.add(next_pos)
-                queue.append((next_pos, count + 1))
-
+    
+    for i in range(1,7):
+        next = now + i
+        if next in laddersnake:
+            next = laddersnake[next]
+    
+        if next not in visited:
+            visited.add(next)
+            queue.append((next,count+1))
 print(mincount)
