5c351af03692cdcc5911d0b69daaf83f86367c26
junhee <112875165+MINLUV@users.noreply.github.com>
2025-05-19 14:16:12 +0900

    Title: 최고의 33위치, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250519/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+maxcnt = 0
+
+for i in range(n-2):
+    for j in range(n-2):
+        cnt = 0
+        for a in range(i,i+3):
+            for b in range(j,j+3):
+                if arr[a][b] == 1:
+                    cnt += 1
+        if cnt > maxcnt:
+n = int(input())
+
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+maxcnt = 0
+
+for i in range(n-2):
+    for j in range(n-2):
+        cnt = 0
+        for a in range(i,i+3):
+
+
+$0
+            maxcnt = cnt
+print(maxcnt)
+                
