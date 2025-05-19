e29a16fde64accf07133a372657b736ae32869f1
junhee <112875165+MINLUV@users.noreply.github.com>
2025-05-19 14:17:10 +0900

    Title: 최고의 33위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250519/최고의 33위치/best-place-of-33.py
+++ 250519/최고의 33위치/best-place-of-33.py
@@ -12,19 +12,6 @@ for i in range(n-2):
                 if arr[a][b] == 1:
                     cnt += 1
         if cnt > maxcnt:
-n = int(input())
-
-arr = [list(map(int,input().split())) for _ in range(n)]
-
-maxcnt = 0
-
-for i in range(n-2):
-    for j in range(n-2):
-        cnt = 0
-        for a in range(i,i+3):
-
-
-$0
             maxcnt = cnt
 print(maxcnt)
                 
